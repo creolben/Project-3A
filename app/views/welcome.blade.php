d90a38847f69e2b8b791b54d246ba0c0bcfc995d
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-05 08:36:26 -0400

    added project folders

---
+++ app/views/welcome.blade.php
@@ -0,0 +1,17 @@
+
+@extends('layout')
+@section('content')
+<div class="row">
+  <div class="col-md-12 backg">
+    <div class="col-md-4 col-md-offset-4 inner col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+      <div class="text-box">
+          <p class="intro">Introducing</p>
+          <h2>Project 3</h2>
+          <h3>Welcome To Project 3</h3>
+          <p>By <span><a href="http://www.html5layouts.com">HTML5 Layouts</a></span></p>
+          <p><a href="#" class="link-button">Tell Me Something</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+@stop
