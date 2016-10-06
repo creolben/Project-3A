69df582a625685c92b73cad1cbc1481718367ba9
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-05 22:38:48 -0400

    added package Lorem ipsum generator

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -1,15 +1,28 @@
 
 @extends('layout')
 @section('content')
+<?php
+$generator = new Badcow\LoremIpsum\Generator();
+$paragraphs = $generator->getParagraphs(5);
+?>
 <div class="row">
   <div class="col-md-12 backg">
-    <div class="col-md-4 col-md-offset-4 inner col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+    <div class="col-md-10 inner col-xs-10 col-xs-offset-1 col-sm-6">
       <div class="text-box">
-          <p class="intro">Introducing</p>
-          <h2>Project 3</h2>
-          <h3>Welcome To Project 3</h3>
-          <p>By <span><a href="http://www.html5layouts.com">HTML5 Layouts</a></span></p>
-          <p><a href="#" class="link-button">Tell Me Something</a></p>
+        <div>
+          <h4>How many paragraphs do you want?</h4>
+          <br>
+          <form class="form-inline">
+            <div class="form-group">
+              <label for="exampleInputName2">Paragraphs</label>
+              <input type="text" class="form-control" id="InputName2" placeholder="5" Value="5">
+            </div>
+            <button type="submit" class="btn btn-default">Generate!</button>
+          </form>
+          <h5>(Max: 99)</h5>
+      </div>
+          <h2 class="intro">Developer's Best Friend</h2>
+          <p><?php echo implode('<p>', $paragraphs);?></p>
       </div>
     </div>
   </div>
