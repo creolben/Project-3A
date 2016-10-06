e478f45fb02c63e3d9e07908ff5882efbdbba2b4
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-05 22:50:35 -0400

    seacond page

--- app/views/second_page.blade.php
+++ app/views/second_page.blade.php
@@ -1,75 +1,32 @@
 
 @extends('layout')
 @section('content')
-    <div class="row features">
-  <div class="col-md-12">
-    <h2 class="text-center features-text">Features</h2>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-pencil"></i>
+<?php
+$generator = new Badcow\LoremIpsum\Generator();
+$paragraphs = $generator->getParagraphs(5);
+?>
+  <div class="row features">
+    <div class="col-md-12">
+      <div class="col-md-12">
+        <div class="col-md-10 inner col-xs-10 col-xs-offset-1 col-sm-6">
+          <div class="text-box">
+            <div>
+              <h4>How many paragraphs do you want?</h4>
+              <br>
+              <form class="form-inline">
+                <div class="form-group">
+                  <label for="exampleInputName2">Paragraphs</label>
+                  <input type="text" class="form-control" id="InputName2" placeholder="5" Value="5">
+                </div>
+                <button type="submit" class="btn btn-default">Generate!</button>
+              </form>
+              <h5>(Max: 99)</h5>
+          </div>
+              <h2 class="intro">Developer's Best Friend</h2>
+              <p><?php echo implode('<p>', $paragraphs);?></p>
+          </div>
         </div>
       </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>Modern Design</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
     </div>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-cog"></i>
-        </div>
-      </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>Easy To Customize</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </div>
-    </div>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-time"></i>
-        </div>
-      </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>Save Time</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, adipiscing elit.</p>
-      </div>
-    </div>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-briefcase"></i>
-        </div>
-      </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>Professional Work</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
-      </div>
-    </div>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-user"></i>
-        </div>
-      </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>User-friendly Interface</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit.</p>
-      </div>
-    </div>
-    <div class="col-md-6 col-sm-12 col-xs-12 icon-box">
-      <div class="col-md-2 col-sm-2 col-xs-12">
-        <div class="iconing">
-          <i class="glyphicon glyphicon-heart"></i>
-        </div>
-      </div>
-      <div class="col-md-9 col-md-offset-1 col-sm-10 col-xs-12 icon-text-box">
-                  <h4>Made With Love</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
-      </div>
-    </div>
-  </div>
 </div>
 @stop
